7b3a4856ceb02979c4cb76bb5d1593add2c2cb8c
Mark Glenn <markglenn@gmail.com>
2026-05-02 17:41:07 -0500

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hrx.MixProject do
     [
       app: :hrx,
       name: "hrx",
-      version: "0.1.2",
+      version: "0.2.0",
       description: description(),
       source_url: "https://github.com/markglenn/hrx",
       homepage_url: "https://github.com/markglenn/hrx",
@@ -37,7 +37,7 @@ defmodule Hrx.MixProject do
     [
       {:nimble_parsec, "~> 1.1"},
       {:credo, "~> 1.2", only: [:dev], runtime: false},
-      {:dialyxir, "~> 1.0.0", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
